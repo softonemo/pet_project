621c2e0193f1e49f958ea46dac550200ece5af57
softonemo <narimannarynbekov@gmail.com>
2024-02-15 22:22:22 +0600

    fullpage

--- src/mainpages/index.jsx
+++ src/mainpages/index.jsx
@@ -2,36 +2,48 @@ import React from 'react';
 import mainVideo from './images/clideo_editor_bae05325d07c4d3c8488d817fe9ee247.gif';
 import girlWith from './images/girl_with_merc.avif';
 import './index.css';
+import ReactFullpage from '@fullpage/react-fullpage'; 
 
 function MainPage() {
   return (
-    <div className="container">
-      <div
-        className="block_with_mainVideo"
-        style={{
-          backgroundImage: `url(${mainVideo})`,
-          backgroundSize: 'cover',
-          scale: '1.2',
-          height: 'calc(100vh)',
-        }}
-      >
-        <div>
-          <h1>ddddddd</h1>
-          <button>dddd</button>
-        </div>
-      </div>
-      <button style={{ zIndex: '10' }}>DISCOVER MORE</button>
-      <div
-        className="welcome_to_beyond"
-        style={{
-          backgroundImage: `url(${girlWith})`,
-          backgroundSize: 'cover',
-          height: '100vh',
-        }}
-      >
-        <h2>ldkkekldkgmlks,ml</h2>
-      </div>
-    </div>
+    <ReactFullpage
+      licenseKey={'YOUR_KEY_HERE'}
+      scrollingSpeed={1000}
+      sectionsColor={['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']}
+      anchors={['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage']}
+      menu='#menu'
+      css3={true}
+      render={({ state, fullpageApi }) => {
+        return (
+          <div id="fullpage">
+            <div
+              className="section block_with_mainVideo"
+              style={{
+                backgroundImage: `url(${mainVideo})`,
+                backgroundSize: 'cover',
+                scale: '1.2',
+                height: 'calc(100vh)',
+              }}
+            >
+              <div>
+                <h1>ddddddd</h1>
+                <button>dddd</button>
+              </div>
+            </div>
+            <div
+              className="section welcome_to_beyond"
+              style={{
+                backgroundImage: `url(${girlWith})`,
+                backgroundSize: 'cover',
+                height: '100vh',
+              }}
+            >
+              <h2>ldkkekldkgmlks,ml</h2>
+            </div>
+          </div>
+        );
+      }}
+    />
   );
 }
 
